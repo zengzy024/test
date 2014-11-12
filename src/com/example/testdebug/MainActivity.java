92eb32181a3c60b65612c3a0d8dd8bd8e1636e8b
zengzy024 <zengzy024@163.com>
2014-11-12 15:36:13 +0800

    banach

--- src/com/example/testdebug/MainActivity.java
+++ src/com/example/testdebug/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testdebug;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity
 {
@@ -11,6 +12,7 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.i("log", "banach");
 	}
 
 	
