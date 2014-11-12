4ec58948093033703bdf66bd45f23d92d5e8ce52
zengzy024 <zengzy024@163.com>
2014-11-12 14:50:50 +0800

    test git function first.

---
+++ src/com/example/testdebug/PluginClass.java
@@ -0,0 +1,16 @@
+package com.example.testdebug;
+
+import com.example.host.Comm;
+
+import android.util.Log;
+
+public class PluginClass implements Comm{
+
+	public PluginClass(){
+		Log.i("Plugin", "PluginClass client initialized");
+	}
+	
+	public int function(int a , int b){
+		return a+b;
+	}
+}
