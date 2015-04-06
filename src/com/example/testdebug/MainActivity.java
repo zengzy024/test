e235680e4a18c9bdb8464242dc49d9eb38fdbc6c
zengzy024 <zengzy024@163.com>
2015-04-06 14:07:54 +0800

    Update MainActivity.java

--- src/com/example/testdebug/MainActivity.java
+++ src/com/example/testdebug/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity
 		Log.i("log", "test git");
 		Log.i("log", "three");
 		Log.i("log", "sourcetree 测试");
+		Log.i("log", "pull 测试");
 		String ces = "只是为了测试git的相应功能实现";
 		Toast.makeText(this, ces, Toast.LENGTH_SHORT).show();;
 	}
